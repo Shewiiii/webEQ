f0dfe9e47696381bf2a45d9dcd3dae6a3f3155d9
Shewiiii <akiempj123@gmail.com>
2024-04-20 20:11:31 +0200

    Add FR

--- scraping_scripts/console.js
+++ scraping_scripts/console.js
@@ -1,14 +1,15 @@
-// script to past in the console to get the array of the active phones (https://crinacle.com/graphs/iems/graphtool/) 
-for (const iem of document.getElementById('GraphTool').contentWindow.allPhones) {
-  if (iem['active'] == true) {
-		var average = [];
-		var left = iem['channels'][0];
-		var right = iem['channels'][1];
+// script to past in the console to get the array of the active phones (https://crinacle.com/graphs/iems/graphtool/)
+for (const iem of document.getElementById("GraphTool").contentWindow
+  .allPhones) {
+  if (iem["active"] == true) {
+    var average = [];
+    var left = iem["channels"][0];
+    var right = iem["channels"][1];
 
-		for(i = 0; i < left.length; i++) {
-			average.push([left[i][0],(left[i][1] + right[i][1]) / 2]);
+    for (i = 0; i < left.length; i++) {
+      average.push([left[i][0], (left[i][1] + right[i][1]) / 2]);
     }
-		var all = [average,iem['dispBrand']+ ' ' +iem['phone']];
+    var all = [average, iem["dispBrand"] + " " + iem["phone"]];
     console.log(all);
   }
 }
